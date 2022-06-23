78179dc3e0a3f29e0960a17d04b78d377371008a
Tolulope Dada <tdada0912@gmail.com>
2022-06-24 00:53:51 +0100

    code optimization

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-window.onscroll = () => {scrollFunction()};
-
+window.onscroll = () => {
+    scrollFunction();
+    skillProgress();
+};
 
 scrollFunction = () => {
 
@@ -9,5 +11,16 @@ scrollFunction = () => {
         topNavs.classList.add("nav-scroll");
     } else {
         topNavs.classList.remove("nav-scroll");
+    }
+}
+
+skillProgress = () => {
+
+    let statisticsProgress = document.querySelector(".statistics-progress");
+    
+    if (document.documentElement.scrollTop > 50) {
+        statisticsProgress.classList.add("skill-progress");
+    } else {
+        statisticsProgress.classList.remove("skill-progress");
     }
 }
